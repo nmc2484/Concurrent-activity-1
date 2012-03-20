ad21572b9791818d8060367db2d02e2c3f017889
Matt Alioto <matthew.j.alioto@gmail.com>
2012-03-20 17:17:15 -0400

    Fixed logic in run(); added comments

--- src/activity_1/Client.java
+++ src/activity_1/Client.java
@@ -8,7 +8,17 @@ public class Client extends Thread {
 	private int nRequests;
 	private long minSleepMillis;
 	private long maxSleepMillis;
-	
+
+	/**
+	 * Fully parameterized construction for a client
+	 * which acquires resources from Banker
+	 * @param name of Client (e.g. client3)
+	 * @param banker in which to register client
+	 * @param nUnits maximum claim Client may make
+	 * @param nRequests number of request/release cycles
+	 * @param minSleepMillis after request/release cycle
+	 * @param maxSleepMillis after request/release cycle
+	 */
 	public Client(String name, Banker banker, int nUnits, int nRequests, long minSleepMillis, long maxSleepMillis) {
 		super(name);
 		this.banker = banker;
@@ -18,28 +28,38 @@ public class Client extends Thread {
 		this.maxSleepMillis = maxSleepMillis;
 	}
 
+	/**
+	 * Run method for Thread subclass.
+	 * 
+	 */
 	public void run() {
-		// "up to" nUnits
-		int claimSize = (int) (1 + (Math.random() * this.nUnits));
-		this.banker.setClaim(claimSize);
+		// Claim nUnits resources		
+		this.banker.setClaim(nUnits);
+		// Cycle nRequests times
 		for (int i = 1; i <= nRequests; i++) {
 			if (banker.remaining() == 0) {
-				banker.release(nUnits);
+				// If bank has no resources left, release
+				// resources claimed by this client
+				banker.release(banker.allocated());
 			} 
 			else if (banker.allocated() == 0){
-				banker.request(nUnits);
+				// If this client has not acquired any resources,
+				// request an amount between 1 and the maximum claim
+				int requestSize = (int) (1 + (Math.random() * this.nUnits));
+				banker.request(requestSize);
 			} else {
-				// Request *some* units
+				// This client has already acquired some resources,
+				// and there is room to acquire some more.
+				// Flip a coin to decide request/release
 				Random r = new Random();
 				boolean probAction = r.nextBoolean();
-				//random 50/50 request or release resources
 				if(probAction){
-					//if release, release at least 1
+					//Release between 1 and numUnitsAllocated units
 					int releaseUnits = (int) (1 + (Math.random() * (banker.allocated() - 1)));
 					banker.release(releaseUnits);
 				}
 				else{
-					//if request, request at least 1
+					//Request between 1 and numUnitsAvailable units
 					int requestUnits = (int) (1 + (Math.random() * (banker.remaining() - 1)));
 					banker.request(requestUnits);
 				}
@@ -48,10 +68,7 @@ public class Client extends Thread {
 				// Thread sleeps for random time between minSleepMillis and maxSleepMillis
 				long randomSleep = (long) (minSleepMillis + (Math.random() * ((maxSleepMillis - minSleepMillis) + 1)));
 				Thread.sleep(randomSleep);
-			} catch (InterruptedException e) {
-				// TODO How to respond?
-				e.printStackTrace();
-			}
+			} catch (InterruptedException ignore) {/**/}
 		}
 		banker.release(nUnits);
 		return;
