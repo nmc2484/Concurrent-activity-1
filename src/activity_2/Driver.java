7884ea15cc00300b7b4c0c36e9f3558656422253
Matt Alioto <matthew.j.alioto@gmail.com>
2012-03-25 17:00:58 -0400

    Set up driver class

--- src/activity_2/Driver.java
+++ src/activity_2/Driver.java
@@ -1,9 +1,36 @@
 package activity_2;
 
+import java.util.ArrayList;
+
 public class Driver {
 
-    public static void main(String[] args){
-        //TODO get this working
+	public static void main(String[] args){
+		//TODO get this working
+		boolean rightHanded = true;
+		int np = 4;
+		int nt = 10;
+		int tm = 0;
+		int em = 0;
 
-    }
+		// Create np forks
+		Fork[] forks = new Fork[np];
+		for (Fork fork : forks) {
+			fork = new Fork();
+		}
+
+		// Create np philosophers and assign forks
+		ArrayList<Philosopher> philosophers = new ArrayList<Philosopher>(np);
+		for (int i = 0; i < np; i++) {
+			int lf = (np + i - 1) % np;
+			Philosopher philosopher;
+			if (!rightHanded && i % 2 == 1) {
+				philosopher = new Philosopher(i, forks[lf], forks[i], false, nt, tm, em);
+			} else {
+				philosopher = new Philosopher(i, forks[lf], forks[i], false, nt, tm, em);
+			}
+			philosophers.add(philosopher);
+		}
+		// Start philosophizing 
+		for (Philosopher philosopher : philosophers) philosopher.start();
+	}
 }
